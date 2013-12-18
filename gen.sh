fd5e658b17739ad0297437e18ee2ba93b14d8003
Micheal Waltz <mwaltz@qualcomm.com>
2013-12-18 13:38:21 -0800

    Download and unzip SteamOSInstaller.zip into BUILD

--- gen.sh
+++ gen.sh
@@ -5,16 +5,62 @@ DISTNAME=alchemist
 ISOPATH=".."
 ISONAME="yeolde.iso"
 
+
 #Check dependencies
-deps="apt-utils xorriso syslinux"
+deps="apt-utils xorriso syslinux rsync wget unzip"
 for dep in ${deps}; do
 	if dpkg-query -s ${dep} >/dev/null 2>&1; then
 	        :
 	else
 		echo "Missing dependency: ${dep}"
 		echo "Install with: sudo apt-get install ${dep}"
-#		exit 1
+		exit 1
 	fi
+done
+
+#Download SteamOSInstaller.zip
+steaminstallfile="SteamOSInstaller.zip"
+steaminstallerurl="http://repo.steampowered.com/download/${steaminstallfile}"
+if [ ! -f ${steaminstallfile} ]; then
+	echo "Downloading ${steaminstallerurl} ..."
+	if wget -q -N ${steaminstallerurl}; then
+		:
+	else
+		echo "Error downloading ${steaminstallerurl}!"
+		exit 1
+	fi
+fi
+
+#Unzip SteamOSInstaller.zip into BUILD
+if unzip ${steaminstallfile} -d ${BUILD}; then
+	:
+else
+	echo "Error unzipping ${steaminstallfile} into ${BUILD}!"
+	exit 1
+fi
+
+#Copy over updated and added debs
+#First remove uneeded debs
+debstoremove="pool/main/x/xserver-xorg-video-vmware/xserver-xorg-video-vmware_12.0.2-1+bsos6_amd64.deb"
+for debremove in ${debstoremove}; do
+	if [ -f ${BUILD}/${debstoremove} ]; then
+		rm -fr "${BUILD}/${debstoremove}"
+	fi
+done
+
+#Rsync over our local pool dir
+pooldir="./pool"
+if rsync -av ${pooldir} ${BUILD} >/dev/null 2>&1; then
+	:
+else
+	echo "Error copying ${pooldir} to ${BUILD}"
+	exit 1
+fi
+
+#Copy over the rest of our modified files
+yeoldfiles="default.preseed isolinux post_install.sh"
+for file in ${yeoldfiles}; do
+	cp -pfr ${file} ${BUILD}
 done
 
 echo "Generating Packages.."
