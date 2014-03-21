32fdbb258707e17f37fc3fd0b600a7ef517df2ed
Jo Shields <directhex@apebox.org>
2014-03-21 14:03:21 +0000

    Oops, wasn't mirroring alchemist_beta

--- archive-mirror.sh
+++ archive-mirror.sh
@@ -2,4 +2,4 @@
 mkdir -p archive-mirror/
 debmirror -p --nosource --keyring=/usr/share/keyrings/valve-archive-keyring.gpg \
 	--rsync-extra=none -s main,contrib,non-free,main/debian-installer -a amd64,i386 -v \
-	-d alchemist --method=http -h repo.steampowered.com -r steamos archive-mirror/
+	-d alchemist,alchemist_beta --method=http -h repo.steampowered.com -r steamos archive-mirror/
