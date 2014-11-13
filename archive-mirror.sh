58a476bf936f5ad86718a9e4c47a5df65d7c593c
Wouter Wijsman <Wouter.Wijsman@snow.nl>
2014-11-13 14:27:23 +0100

    fixed archive-mirror.sh to use valve key in the git repo

--- archive-mirror.sh
+++ archive-mirror.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p archive-mirror/
-debmirror -p --nosource --keyring=/usr/share/keyrings/valve-archive-keyring.gpg \
+debmirror -p --nosource --keyring=./valve-archive-keyring.gpg \
 	--rsync-extra=none -s main,contrib,non-free,main/debian-installer -a amd64,i386 -v \
 	-d alchemist,alchemist_beta --method=http -h repo.steampowered.com -r steamos archive-mirror/
