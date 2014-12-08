1f3109b233f7ff6eac497ec7a9019d11d2223d8d
Wouter Wijsman <wwijsman@live.nl>
2014-12-08 15:12:03 +0100

    Updated for new directory layout archive-mirror
    Required for obsolete report to function after switching archive-mirror.sh to apt-mirror.

--- gen.sh
+++ gen.sh
@@ -12,7 +12,7 @@ UPSTREAMURL="http://repo.steampowered.com"
 STEAMINSTALLFILE="SteamOSDVD.iso"
 MD5SUMFILE="MD5SUMS"
 KNOWNINSTALLER="e0583f17ea8f6d5307b74a0d28a99a5e"
-REPODIR="./archive-mirror/"
+REPODIR="./archive-mirror/mirror/repo.steampowered.com/steamos"
 
 #Show how to use gen.sh
 usage ( )
